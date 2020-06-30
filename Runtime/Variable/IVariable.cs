007d548d4fb859f1a7f82db0eed110d964fea4ad
Ta David Yu <d366095@gmail.com>
2020-06-30 16:20:53 +0200

    Added IVariable interface

---
+++ Runtime/Variable/IVariable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DYLogic
+{
+
+    public interface IVariable
+    {
+        string Name { get; }
+        int Test { get; }
+    }
+}
